974d55accea0699dd04c26e37eb4bf7ce223fcc2
Kai Suzuki <ifuymak@gmail.com>
2021-12-01 15:41:49 +0000

    add keras.sequential model

---
+++ train_kerasModel.py
@@ -0,0 +1,178 @@
+# import the necessary packages
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.applications import MobileNetV2
+from tensorflow.keras.layers import AveragePooling2D
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Input
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.preprocessing.image import load_img
+from tensorflow.keras.utils import to_categorical
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from imutils import paths
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+#for detector
+import cv2
+import imutils
+import time
+from imutils.video import VideoStream
+
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+
+
+
+# initialize the initial learning rate, number of epochs to train for,
+# and batch size
+INIT_LR = 1e-4
+EPOCHS = 20
+BS = 32
+
+DIRECTORY = "dataset"
+DIRECTORY = "mini_dataset"
+CATEGORIES = ["with_mask", "without_mask"]
+
+# grab the list of images in our dataset directory, then initialize
+# the list of data (i.e., images) and class images
+print("[INFO] loading images...")
+
+data = []
+labels = []
+
+for category in CATEGORIES:
+    path = os.path.join(DIRECTORY, category)
+    for img in os.listdir(path):
+    	img_path = os.path.join(path, img)
+    	image = load_img(img_path, target_size=(224, 224))
+    	image = img_to_array(image)
+    	image = preprocess_input(image)
+
+    	data.append(image)
+    	labels.append(category)
+
+# perform one-hot encoding on the labels
+lb = LabelBinarizer()
+labels = lb.fit_transform(labels)
+labels = to_categorical(labels)
+
+data = np.array(data, dtype="float32")
+labels = np.array(labels)
+
+(trainX, testX, trainY, testY) = train_test_split(data, labels,
+	test_size=0.20, stratify=labels, random_state=42)
+
+# construct the training image generator for data augmentation
+aug = ImageDataGenerator(
+	rotation_range=20,
+	zoom_range=0.15,
+	width_shift_range=0.2,
+	height_shift_range=0.2,
+	shear_range=0.15,
+	horizontal_flip=True,
+	fill_mode="nearest")
+
+
+
+from tensorflow import keras
+from keras.layers import Conv2D, MaxPooling2D, LeakyReLU
+from tensorflow.keras import layers, regularizers
+# Model / data parameters
+num_classes = 2
+input_shape = (224, 224, 3)
+
+
+#------------------------------------------------------------------------
+#ver1
+model = keras.Sequential()
+model.add(Conv2D(16, (3,3), padding='same', input_shape=trainX.shape[1:],activation='relu'))
+model.add(Conv2D(16, (3,3), strides=(2,2), padding='same', activation='relu'))
+model.add(Conv2D(32, (3,3), padding='same', activation='relu'))
+model.add(Conv2D(32, (3,3), strides=(2,2), padding='same', activation='relu'))
+model.add(Dropout(0.5))
+model.add(Flatten())
+model.add(Dense(num_classes, activation='softmax',kernel_regularizer=regularizers.l1(0.0001))) #change the weight parameter of L1(bigger and smaller including 0)
+model.compile(loss="categorical_crossentropy", optimizer='adam', metrics=["accuracy"])
+model.summary()
+
+# #ver2
+# model = keras.Sequential()
+# model.add(Conv2D(16, (3,3), padding='same', input_shape=trainX.shape[1:],activation='relu'))
+# model.add(MaxPooling2D(pool_size=(2,2)))
+# model.add(Conv2D(32, (3,3), padding='same', activation='relu'))
+# model.add(MaxPooling2D(pool_size=(2,2)))
+# model.add(Dropout(0.5))
+# model.add(Flatten())
+# model.add(Dense(num_classes, activation='softmax',kernel_regularizer=regularizers.l1(0.0001))) #change the weight parameter of L1(bigger and smaller including 0)
+# model.compile(loss="categorical_crossentropy", optimizer='adam', metrics=["accuracy"])
+# model.summary()
+
+#ver3
+# model = keras.Sequential()
+# model.add(Conv2D(16, (3,3), padding='same', input_shape=trainX.shape[1:],activation='relu'))
+# model.add(Conv2D(16, (3,3), strides=(2,2), padding='same', activation='relu'))
+# model.add(Conv2D(16, (3,3), strides=(2,2), padding='same', activation='relu'))
+# model.add(Conv2D(16, (3,3), strides=(2,2), padding='same', activation='relu'))
+# model.add(Conv2D(16, (3,3), strides=(2,2), padding='same', activation='relu'))
+# #model.add(MaxPooling2D(pool_size=(2,2)))
+# model.add(Conv2D(32, (3,3), padding='same', activation='relu'))
+# model.add(Conv2D(32, (3,3), strides=(2,2), padding='same', activation='relu'))
+# model.add(Conv2D(16, (3,3), strides=(2,2), padding='same', activation='relu'))
+# #model.add(MaxPooling2D(pool_size=(2,2)))
+# model.add(Dropout(0.5))
+# model.add(Flatten())
+# model.add(Dense(num_classes, activation='softmax',kernel_regularizer=regularizers.l1(0.0001))) #change the weight parameter of L1(bigger and smaller including 0)
+# model.compile(loss="categorical_crossentropy", optimizer='adam', metrics=["accuracy"])
+# model.summary()
+#------------------------------------------------------------------------
+
+batch_size = 128
+epochs = 20
+
+print("orig x_train shape:", trainX.shape)
+print("orig y_train shape:", trainY.shape)
+H = model.fit(trainX, trainY, batch_size=batch_size, epochs=epochs, validation_split=0.1)
+
+
+# make predictions on the testing set
+print("[INFO] evaluating network...")
+predIdxs = model.predict(testX, batch_size=BS)
+
+# for each image in the testing set we need to find the index of the
+# label with corresponding largest predicted probability
+predIdxs = np.argmax(predIdxs, axis=1)
+
+# show a nicely formatted classification report
+print(classification_report(testY.argmax(axis=1), predIdxs,
+	target_names=lb.classes_))
+
+# serialize the model to disk
+print("[INFO] saving mask detector model...")
+model.save("mask_detector.model", save_format="h5")
+
+# plot the training loss and accuracy
+N = EPOCHS
+plt.style.use("ggplot")
+plt.figure()
+plt.plot(np.arange(0, N), H.history["loss"], label="train_loss")
+plt.plot(np.arange(0, N), H.history["val_loss"], label="val_loss")
+plt.plot(np.arange(0, N), H.history["accuracy"], label="train_acc")
+plt.plot(np.arange(0, N), H.history["val_accuracy"], label="val_acc")
+plt.title("Training Loss and Accuracy")
+plt.xlabel("Epoch #")
+plt.ylabel("Loss/Accuracy")
+plt.legend(loc="lower left")
+plt.savefig("plot.png")
+plt.show()
+
+
+
