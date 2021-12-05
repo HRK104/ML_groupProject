39d5adc894711d8e4a8673466c6a938a7574b7e8
Suman Neupane <neupanes@tcd.ie>
2021-12-05 16:30:00 +0000

    Update train_kerasModel.py

--- train_kerasModel.py
+++ train_kerasModel.py
@@ -1,15 +1,11 @@
 # import the necessary packages
-from tensorflow.keras import layers, regularizers
-from keras.layers import Conv2D, MaxPooling2D, LeakyReLU
+from tensorflow.keras import regularizers
+from keras.layers import Conv2D
 from tensorflow import keras
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.applications import MobileNetV2
-from tensorflow.keras.layers import AveragePooling2D
 from tensorflow.keras.layers import Dropout
 from tensorflow.keras.layers import Flatten
 from tensorflow.keras.layers import Dense
-from tensorflow.keras.layers import Input
-from tensorflow.keras.models import Model
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 from tensorflow.keras.preprocessing.image import img_to_array
@@ -17,26 +13,15 @@ from tensorflow.keras.preprocessing.image import load_img
 from tensorflow.keras.utils import to_categorical
 from sklearn.preprocessing import LabelBinarizer
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report
-from imutils import paths
+from sklearn.metrics import classification_report, confusion_matrix
 import matplotlib.pyplot as plt
 import numpy as np
 import os
 
-# for detector
-import cv2
-import imutils
-import time
-from imutils.video import VideoStream
-
-import tensorflow as tf
-import numpy as np
-from PIL import Image
-
 # initialize the initial learning rate, number of epochs to train for,
 # and batch size
 INIT_LR = 1e-4
-EPOCHS = 50
+EPOCHS = 20
 BS = 32
 
 DIRECTORY = "dataset"
@@ -44,7 +29,7 @@ CATEGORIES = ["with_mask", "without_mask"]
 
 # grab the list of images in our dataset directory, then initialize
 # the list of data (i.e., images) and class images
-print("[INFO] loading images...")
+print("Loading images...")
 
 data = []
 labels = []
@@ -69,7 +54,7 @@ data = np.array(data, dtype="float32")
 labels = np.array(labels)
 
 (trainX, testX, trainY, testY) = train_test_split(data, labels,
-                                                  test_size=0.20, stratify=labels, random_state=42)
+                                                  test_size=0.20, stratify=labels, random_state=50)
 
 # construct the training image generator for data augmentation
 aug = ImageDataGenerator(
@@ -117,41 +102,52 @@ model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
 model.add(Dropout(0.5))
 model.add(Flatten())
 model.add(Dense(num_classes, activation='softmax', kernel_regularizer=regularizers.l1(
-    0.00001)))  # change the weight parameter of L1(bigger and smaller including 0)
+    0.001)))  # change the weight parameter of L1(bigger and smaller including 0)
 model.compile(loss="categorical_crossentropy",
               optimizer='adam', metrics=["accuracy"])
+
+opt = Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS)
+model.compile(loss="binary_crossentropy", optimizer=opt,
+	metrics=["accuracy"])
+
 model.summary()
 
 History = model.fit(trainX, trainY, batch_size=BS, epochs=EPOCHS, steps_per_epoch=int(
     np.ceil(trainX.shape[0]//BS)), validation_split=0.1)
 
 # make predictions on the testing set
-print("[INFO] evaluating network...")
+print("Making predictions on the test set...")
 predIdxs = model.predict(testX, batch_size=BS)
 
 # for each image in the testing set we need to find the index of the
 # label with corresponding largest predicted probability
 predIdxs = np.argmax(predIdxs, axis=1)
 
-# show a nicely formatted classification report
-print(classification_report(testY.argmax(axis=1), predIdxs,
-                            target_names=lb.classes_))
-
 # serialize the model to disk
-print("[INFO] saving mask detector model...")
-model.save("mask_detector.model", save_format="h5")
+print("Saving the current model...")
+model.save("detect_mask.model", save_format="h5")
 
 # plot the training loss and accuracy
-N = EPOCHS
-plt.style.use("ggplot")
+N = N = len(History.history["loss"])
+plt.style.use("bmh")
 plt.figure()
 plt.plot(np.arange(0, N), History.history["loss"], label="train_loss")
 plt.plot(np.arange(0, N), History.history["val_loss"], label="val_loss")
 plt.plot(np.arange(0, N), History.history["accuracy"], label="train_acc")
 plt.plot(np.arange(0, N), History.history["val_accuracy"], label="val_acc")
 plt.title("Training Loss and Accuracy")
-plt.xlabel("Epoch #")
+plt.xlabel("Number of Epoch")
 plt.ylabel("Loss/Accuracy")
 plt.legend(loc="lower left")
 plt.savefig("plot.png")
 plt.show()
+
+# printing classification report
+print("Classification Report: ")
+print(classification_report(testY.argmax(axis=1), predIdxs,
+                            target_names=lb.classes_))
+
+# printing the confusion matrix
+conf_matrix = confusion_matrix(testY.argmax(axis=1), predIdxs)
+print("Confusion Matrix: ")
+print(conf_matrix)
